0cc710d81c461227d9aeb71d0207234a0e8473bb
Alawar Developer (pv01) <noc@alawar.com>
2012-11-06 16:20:03 +0700

    Hotfix v.0.5.1

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -f $HOST_CONFIG_DIR/pvetools.conf ]; then
-	cp -f $HOST_CONFIG_DIR/pvetools.conf `dirname $0`/pvetools.conf
+if [ -f /usr/local/etc/pvetools/pvetools.conf ]; then
+	cp -f /usr/local/etc/pvetools/pvetools.conf `dirname $0`/pvetools.conf
+else
+	touch `dirname $0`/pvetools.conf
 fi
 
 . `dirname $0`/pvetools.conf.default
@@ -18,7 +20,7 @@ if [ ! -L /usr/local/etc/pvetools ]; then
 	ln -s $HOST_CONFIG_DIR /usr/local/etc/pvetools
 fi
 
-touch $HOST_CONFIG_DIR/pvetools.conf
+cp -f `dirname $0`/pvetools.conf $HOST_CONFIG_DIR/pvetools.conf
 
 mkdir -p $HOST_INSTALL_DIR
 
@@ -29,3 +31,5 @@ cp /etc/vz/conf/vps.mount /etc/vz/conf/vps.umount
 
 ln -s $HOST_INSTALL_DIR/bin/start_all_vms.sh /usr/local/bin/${APP_PREFIX}start_all_vms.sh
 ln -s $HOST_INSTALL_DIR/bin/stop_all_vms.sh /usr/local/bin/${APP_PREFIX}stop_all_vms.sh
+
+rm -f `dirname $0`/pvetools.conf
