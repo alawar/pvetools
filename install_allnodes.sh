9d1e37b278008f3ed5da7de8c4b38cc49ac5df53
Alawar Developer (pv01) <noc@alawar.com>
2012-12-13 17:17:25 +0700

    Added  DIR_TO_SYNC

--- install_allnodes.sh
+++ install_allnodes.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f `dirname $0`/pvetools.conf ]; then
 	if [ -f /usr/local/etc/pvetools/pvetools.conf ]; then
+		. /usr/local/etc/pvetools/pvetools.conf
 		cp -f /usr/local/etc/pvetools/pvetools.conf `dirname $0`/pvetools.conf
 	else
 		touch `dirname $0`/pvetools.conf
@@ -20,6 +21,14 @@ for i in $NODES; do
 	TEMP_DIR=`ssh $i mktemp -d --suffix _pve`
 	if [ "$TEMP_DIR" ]; then
 		rsync -a --delete --exclude '.git' $SRC_DIR/ $i:$TEMP_DIR
+		if [ "$DIR_TO_SYNC" ]; then
+			if [ -d $DIR_TO_SYNC ]; then
+				ssh $i mkdir -p $DIR_TO_SYNC
+				rsync -a --delete $DIR_TO_SYNC/ $i:$DIR_TO_SYNC
+			else
+				echo "Directory $DIR_TO_SYNC is not exists!"
+			fi
+		fi
 		ssh $i $TEMP_DIR/install.sh
 		ssh $i rm -rf $TEMP_DIR
 	else
